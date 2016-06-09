a0664f47130e0bf8bbf4a44dcf0e84b36f9f4cb0
Anthony Ho <ahomnky11@gmail.com>
2016-06-09 21:15:06 +0100

    add friendship model

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'my_portfolio', to: "users#my_portfolio"
   get 'search_stocks', to: "stocks#search"
+  get 'my_friends', to: "users#my_friends"
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
